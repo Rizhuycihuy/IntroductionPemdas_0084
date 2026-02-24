5385a929f188154c0c986b9792afcc1c684f61cb
Rizhuycihuy <rizki003131@gmail.com>
2026-02-24 16:03:14 +0700

    pesan

---
+++ boolean.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  bool cek = true;
+  char jk;
+  int angka = 10;
+  float tinggi = 180.5;
+  double harga = 200000.00;
+  string  nama = "rizki";
+
+  jk = 'L';
+  cout << "Selamat siang" << endl;
+  cout << "di ti umy\n";
+  cout << "yogyakarta\n";
+  
+  nama =  "rizki";
+  cout << "nama saya : "  << nama;
+}
---
+++ umur.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+int umur;
+
+umur = 19;
+cout << "rizki" << endl;
+cout << 2006  <<endl;
+cout <<endl;
+
+
+}
